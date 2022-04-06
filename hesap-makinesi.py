3ca73a012b3cf00b99305967f2a538c9e482881e
nxjosephofficial <61632416+nxjosephofficial@users.noreply.github.com>
2022-04-06 22:57:14 +0300

    Calculator / Hesap makinesi

---
+++ hesap-makinesi.py
@@ -0,0 +1,14 @@
+# Hesap Makinesi
+a = int(input("Ilk sayiyi giriniz."+'\n'))
+b = int(input("Ilk sayiyi giriniz."+'\n'))
+c = input("Islem turunu seciniz."+'\n')
+if c == "+":
+    print(a+b)
+if c == "-":
+    print(a-b)
+if c == "*":
+    print(a*b)
+if c == "/":
+    print(a/b)
+else:
+    print("Oops! bir seyler yanlis gitti.")
